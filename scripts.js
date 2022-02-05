85ab0ac628c217ed3bf8ed96b6491e0fe05fc1aa
vitorportes <vitorportnoy@gmail.com>
2022-02-05 09:11:05 -0300

    basic layout with one template card

---
+++ scripts.js
@@ -0,0 +1,6 @@
+
+// let numberOfCards = parseInt (prompt ("Escolha o número de cartas (de 4 a 14)"))
+// while (numberOfCards < 4 || (numberOfCards % 2) != 0 || numberOfCards > 14) {
+//     numberOfCards = parseInt (prompt ("Escolha o número de cartas (de 4 a 14)"))
+// }
+
